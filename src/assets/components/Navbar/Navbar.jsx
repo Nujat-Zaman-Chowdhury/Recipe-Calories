c6e7d5f4b2a1f71b1965f320990525cea438ae52
Nujat-Zaman-Chowdhury <nujattazin@gmail.com>
2024-03-16 05:50:54 +0600

    navbar responsive done

--- src/assets/components/Navbar/Navbar.jsx
+++ src/assets/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className="navbar bg-base-100">
         <div className="navbar-start">
           <div className="dropdown">
-            <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden">
+            <div tabIndex="0" role="button" className="btn btn-ghost lg:hidden pl-0">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-5 w-5"
@@ -30,9 +30,12 @@ const Navbar = () => {
               <li><a href="">Recipes</a></li>
               <li><a href="">About</a></li>
               <li><a href="">Search</a></li>
+              <div className="w-12 h-12 rounded-full bg-[#0BE58A] p-3 hidden  lg:flex justify-center items-center">
+            <FaRegCircleUser className="text-2xl"></FaRegCircleUser>
+            </div>
             </ul>
           </div>
-          <a className="text-3xl font-bold">Recipe Calories</a>
+          <a className="text-2xl lg:text-3xl font-bold">Recipe Calories</a>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">
@@ -44,11 +47,11 @@ const Navbar = () => {
         </div>
         <div className="navbar-end">
           <div className="flex items-center gap-2">
-            <div className="w-[260px] bg-[#150B2B0D] rounded-full flex items-center px-6 py-3 gap-3">
+            <div className="lg:w-[260px] bg-[#150B2B0D] rounded-full flex items-center px-6 py-3 gap-3">
                 <CiSearch />
                 <span>Search</span>
             </div>
-            <div className="w-12 h-12 rounded-full bg-[#0BE58A] p-3 flex justify-center items-center">
+            <div className="w-12 h-12 rounded-full bg-[#0BE58A] p-3 hidden  lg:flex justify-center items-center">
             <FaRegCircleUser className="text-2xl"></FaRegCircleUser>
             </div>
           </div>
@@ -56,12 +59,12 @@ const Navbar = () => {
       </div>
     
         {/* banner */}
-        <div className="mt-7 w-full h-[500px] rounded-2xl">
+        <div className="mt-7 w-full lg:h-[500px] rounded-2xl">
             <div className="bg-[url('./assets/images/banner.jpeg')] w-full h-full bg-cover bg-no-repeat rounded-2xl">
-                <div className="flex justify-center items-center py-24 space-y-4">
-                    <div className="text-white text-center">
-                    <h3 className="text-[52px] font-bold ">Discover an exceptional cooking <br></br> class tailored for you!</h3>
-                    <p className="text-lg w-[933px] mx-auto my-4">Experience Culinary Excellence at Our Chef's Table.Indulge in Exquisite Creations Crafted by Master Chefs.Savor Every Bite, Every Moment, Every Memory</p>
+                <div className="flex justify-center items-center w-full h-full px-3 lg:px-0 py-16 lg:py-24 space-y-4">
+                    <div className="text-white text-center  mx-auto">
+                    <h3 className="text-3xl lg:text-6xl font-bold leading-10">Discover an exceptional cooking <br></br> class tailored for you!</h3>
+                    <p className="text-lg lg:w-[933px] mx-auto my-4">Experience Culinary Excellence at Our Chef's Table.Indulge in Exquisite Creations Crafted by Master Chefs.Savor Every Bite, Every Moment, Every Memory</p>
                     <div className="mt-5 flex gap-4 justify-center items-center">
                     <button className="btn bg-[#0BE58A] border-0 text-[#150B2B] text-xl font-semibold rounded-full">Explore Now</button>
                     <button className="btn bg-transparent text-white border border-white rounded-full text-xl font-semibold">Our Feedback</button>
